957c43db5c8ca508894e8bd675ef80c15366b1b8
BogdanT05 <bogdantodorovic005@gmail.com>
2024-11-18 18:58:38 +0100

    Zadatak 1.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+
+N = int(input("Unesi broj elemenata niza: "))
+lista = []
+rezultat = []
+for i in range(N):
+    lista.append(int(input("Unesi element niza: ")))
+
+min_el = min(lista)
+
+for i in range(len(lista)-2):
+    trojka = lista[i:i+3]
+
+    if min_el in trojka:
+        rezultat.append(trojka)
+
+print(rezultat)
+
+
