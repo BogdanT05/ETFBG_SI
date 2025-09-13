239f17a6282101a0928dc2959cc49b3f56e0e9ed
BogdanT05 <bogdantodorovic005@gmail.com>
2025-09-13 22:11:37 +0200

    Dodati komentari za jul 2020/2021 z1

--- I GODINA/P2/ZADACI SA ISPITA/Ispit/JUL2020_2021/Z1/main.c
+++ I GODINA/P2/ZADACI SA ISPITA/Ispit/JUL2020_2021/Z1/main.c
@@ -4,6 +4,7 @@
 int **read_data() {
     int **matrix = NULL;
     int c, p, d, i = 0;
+    // pravimo matricu sa nepoznatim brojem vrsta i tri kolone
     while (scanf("%d %d %d\n", &c, &p, &d) == 3) {
         matrix = realloc(matrix, (i+1)*sizeof(*matrix));
         matrix[i] = malloc(3*sizeof(*matrix[i]));
@@ -12,12 +13,14 @@ int **read_data() {
         matrix[i][2] = d;
         i++;
     }
+    // zadnja vrsta matrice treba da bude NULL
     matrix = realloc(matrix, (i+2)*sizeof(*matrix));
     matrix[i+1] = NULL;
     return matrix;
 }
 
 void sort_data(int **data) {
+    // bubble sort po kapacitetu sale tj elementima u drugoj koloni
     for (int i = 0; data[i] != NULL; i++) {
         for (int j = i; data[j] != NULL; j++) {
             if (data[i][1] > data[j][1]) {
@@ -31,19 +34,26 @@ void sort_data(int **data) {
 
 void process_data(int **data) {
     int len = 0;
+    // Pronalazimo duzinu matrice
     while (data[len] != NULL) len++;
 
     sort_data(data);
     for (int i = 0; i < len; i++) {
         for (int j = 0; j < len; j++) {
+            // ako je kapacitet dovoljan onda uklanjamo vrstu iz koje
+            // smo prebacili studente i ubacujemo ih u novu salu/vrstu
             if ((data[i][2] + data[j][2]) <= data[j][1] && (i != j)) {
                 data[j][2] = data[i][2] + data[j][2];
                 free(data[i]);
+                // brisemo tu kolonu
                 for (int n = i; n < len; n++) {
                     data[n] = data[n+1];
                 }
+                // smanjujemo duzinu matrice
                 len--;
+                // vracamo se jedan korak unazad mozda opet treba da izvrsimo obradu nad novom vrstom
                 i--;
+                // iskacemo ako smo zavrsili jedan ciklus brisanja
                 break;
             }
         }
