2c58845973bfb4bef4402c4b83e9d9508e8e965c
Bogdan <bogdantodorovic005@gmail.com>
2026-01-24 03:09:46 +0100

    Add interface for sorting algorithms

--- II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sorting Alghoritms/Sorting_Algorithm.h
+++ II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sorting Alghoritms/Sorting_Algorithm.h
@@ -1,9 +1,13 @@
-#ifndef DZ2_SORTING_ALGHORITM_H
-#define DZ2_SORTING_ALGHORITM_H
+#ifndef SORTING_SORTING_ALGORITHM_H
+#define SORTING_SORTING_ALGORITHM_H
+#include "Data Arrays/Data_Array.h"
 
-
+//Abstract class that will be parental class to all sorting Algorithms
 class Sorting_Algorithm {
+public:
+    virtual void sort(Data_Array &data) = 0;
+    virtual ~Sorting_Algorithm() = default;
 };
 
 
-#endif //DZ2_SORTING_ALGHORITM_H
+#endif //SORTING_SORTING_ALGORITHM_H
