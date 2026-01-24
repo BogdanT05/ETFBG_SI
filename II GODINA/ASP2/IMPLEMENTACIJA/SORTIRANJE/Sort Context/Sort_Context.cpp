8f7285836155e7722132902a15145d20a0e05e53
Bogdan <bogdantodorovic005@gmail.com>
2026-01-24 03:27:15 +0100

    Add Sorting Context to connect Data Array and Sorting Algorithm

--- II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sort Context/Sort_Context.cpp
+++ II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sort Context/Sort_Context.cpp
@@ -1,5 +1,15 @@
-//
-// Created by bgdnt on 1/24/2026.
-//
+#include "Sort_Context.h"
 
-#include "../Sort_Context.h"
+Sort_Context::Sort_Context(Data_Array *data, Sorting_Algorithm *algorithm) : data(data), algorithm(algorithm){}
+
+void Sort_Context::sort() const {
+    algorithm->sort(*data);
+}
+
+void Sort_Context::set_algorithm(Sorting_Algorithm *algorithm_) {
+    algorithm = algorithm_;
+}
+
+void Sort_Context::set_data(Data_Array *data_) {
+    data = data_;
+}
--- II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sort Context/Sort_Context.h
+++ II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sort Context/Sort_Context.h
@@ -1,8 +1,18 @@
 #ifndef SORTING_SORT_CONTEXT_H
 #define SORTING_SORT_CONTEXT_H
-
+#include <Data Arrays/Data_Array.h>
+#include <Sorting Alghoritms/Sorting_Algorithm.h>
 
 class Sort_Context {
+private:
+    Data_Array *data;
+    Sorting_Algorithm *algorithm;
+public:
+    Sort_Context(Data_Array *data, Sorting_Algorithm *algorithm);
+
+    void sort() const;
+    void set_algorithm(Sorting_Algorithm* algorithm_);
+    void set_data(Data_Array *data_);
 };
 
 
