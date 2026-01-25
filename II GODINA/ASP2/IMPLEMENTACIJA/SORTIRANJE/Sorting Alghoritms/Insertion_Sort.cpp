9e74cbe617c2b51b7605bdfe96d98786428ada25
Bogdan <bogdantodorovic005@gmail.com>
2026-01-25 03:53:29 +0100

    Implement Insertion Sort

--- II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sorting Alghoritms/Insertion_Sort.cpp
+++ II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sorting Alghoritms/Insertion_Sort.cpp
@@ -1,1 +1,19 @@
 #include "Insertion_Sort.h"
+
+// Insertion sort, works by dividing array in two sections, sorted and unsorted.
+// Sorted section is gradually increasing in every iteration of outer for loop.
+// First element of unsorted section is key element and we are finding his correct position.
+// By comparing it with elements of sorted section and moving them accordingly until we find right positon.
+// We are repeating this process until we sort whole array.
+void Insertion_Sort::sort(Data_Array &array) {
+    for (int i = 1; i < array.get_size(); i++) {
+        int key = array.get(i);
+        int j = i - 1;
+
+        while (j > -1 && array.get(j) > key) {
+            array.set(j+1, array.get(j));
+            j = j - 1;
+        }
+        array.set(j+1, key);
+    }
+}
--- II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sorting Alghoritms/Insertion_Sort.h
+++ II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sorting Alghoritms/Insertion_Sort.h
@@ -1,8 +1,10 @@
 #ifndef SORTING_INSERTION_SORT_H
 #define SORTING_INSERTION_SORT_H
+#include "Sorting_Algorithm.h"
 
 
-class Insertion_Sort {
+class Insertion_Sort : Sorting_Algorithm {
+    void sort(Data_Array &array) override;
 };
 
 
