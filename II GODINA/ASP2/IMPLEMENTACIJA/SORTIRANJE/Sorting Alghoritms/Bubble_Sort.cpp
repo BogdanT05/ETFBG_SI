a9219c3e002db283d48077894a02ee2689b702db
Bogdan <bogdantodorovic005@gmail.com>
2026-01-25 02:58:22 +0100

    Implement Bubble Sort

--- II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sorting Alghoritms/Bubble_Sort.cpp
+++ II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sorting Alghoritms/Bubble_Sort.cpp
@@ -1,1 +1,13 @@
 #include "Bubble_Sort.h"
+
+// Bubble sort works by comparing 2 adjacent elements and propagates biggest of
+// Remaining elements to the end/final position
+void Bubble_Sort::sort(Data_Array &array) {
+    for (int i = 0; i < array.get_size()-1; i++) {
+        for (int j = 0; j < array.get_size()-1-i; j++) {
+            if (array.compare(j, j+1)) { // array[j] > array[j+1]
+                array.swap(j, j+1);
+            }
+        }
+    }
+}
--- II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sorting Alghoritms/Bubble_Sort.h
+++ II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sorting Alghoritms/Bubble_Sort.h
@@ -1,8 +1,11 @@
 #ifndef SORTING_BUBBLE_SORT_H
 #define SORTING_BUBBLE_SORT_H
+#include "Sorting_Algorithm.h"
 
 
-class Bubble_Sort {
+class Bubble_Sort : Sorting_Algorithm{
+public:
+    void sort(Data_Array &array) override;
 };
 
 
