2a984f29d608a234c483c642017988228c1fb0bb
Bogdan <bogdantodorovic005@gmail.com>
2026-01-27 00:07:41 +0100

    Implement Heap Sort

--- II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sorting Alghoritms/Heap_Sort.cpp
+++ II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sorting Alghoritms/Heap_Sort.cpp
@@ -1,1 +1,32 @@
 #include "Heap_Sort.h"
+// Heap Sort is based on heap structure, where parent value is always greater than its sons
+// So to sort array we take the root of heap which and send it to the back of array
+// And then readjust the heap and repeat that proces until whole array is sorted
+void Heap_Sort::sort(Data_Array &array) {
+    int heap_size = array.get_size();
+    for (int i = array.get_size()-1; i > 0; i--) {
+        array.swap(0, i);
+        heapify(array, --heap_size, 0);
+    }
+}
+
+void Heap_Sort::make_heap(Data_Array &array) {
+    for (int i = array.get_size()/2-1; i >= 0; i--)
+        heapify(array, array.get_size(), i);
+}
+
+void Heap_Sort::heapify(Data_Array &array, int heap_size, int index) {
+    while (true) {
+        int largest = index;
+        int left = 2*largest + 1;
+        int right = 2*largest + 2;
+
+        if (left < heap_size && array.get(left) > array.get(largest)) largest = left;
+        if (right < heap_size && array.get(right) > array.get(largest)) largest = right;
+        if (largest == index) return;
+
+        array.swap(largest, index);
+        index = largest;
+    }
+}
+
--- II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sorting Alghoritms/Heap_Sort.h
+++ II GODINA/ASP2/IMPLEMENTACIJA/SORTIRANJE/Sorting Alghoritms/Heap_Sort.h
@@ -1,8 +1,13 @@
 #ifndef SORTING_HEAP_SORT_H
 #define SORTING_HEAP_SORT_H
+#include "Sorting_Algorithm.h"
 
 
-class Heap_Sort {
+class Heap_Sort : Sorting_Algorithm {
+public:
+    void sort(Data_Array &array) override;
+    static void make_heap(Data_Array &array) ;
+    static void heapify(Data_Array &array, int heap_size, int index) ;
 };
 
 
