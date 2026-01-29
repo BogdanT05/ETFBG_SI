7f4526b23cd2676528236e3691e5523ac7d78a6e
Bogdan <bogdantodorovic005@gmail.com>
2026-01-29 17:03:33 +0100

    Implement Interpreter Class, main loop and error handling

--- II GODINA/OOP1/PROJEKAT_25_26/src/interpreter/Interpreter.cpp
+++ II GODINA/OOP1/PROJEKAT_25_26/src/interpreter/Interpreter.cpp
@@ -1,1 +1,41 @@
 #include "Interpreter.h"
+#include <algorithm>
+#include <vector>
+#include "Token.h"
+
+Interpreter::Interpreter(Tokenizer &tokenizer, Parser &parser, std::string prompt) :
+tokenizer(tokenizer),
+parser(parser),
+prompt(std::move(prompt))
+{}
+
+void Interpreter::set_prompt(const std::string& new_prompt) {
+    prompt = new_prompt;
+}
+
+void Interpreter::run() {
+    while (true) {
+        std::string raw_command;
+        std::cout << prompt;
+        if (!std::getline(std::cin, raw_command)) break;
+
+        if (raw_command.empty() || is_blank(raw_command)) continue;
+
+        try{
+            std::vector<Token> tokens = tokenizer.tokenize(raw_command);
+            auto execution_plan = parser.parse(tokens);
+            execution_plan->execute();
+        }
+        catch (const Error &er) {
+            print_error(er);
+        }
+    }
+}
+
+void Interpreter::print_error(const Error &error) {
+    error.print();
+}
+
+bool Interpreter::is_blank(const std::string &string) {
+    return std::all_of(string.begin(), string.end(), [](const unsigned char c){return std::isspace(c);});
+}
--- II GODINA/OOP1/PROJEKAT_25_26/src/interpreter/Interpreter.h
+++ II GODINA/OOP1/PROJEKAT_25_26/src/interpreter/Interpreter.h
@@ -1,9 +1,25 @@
 #ifndef PROJEKAT2526_INTERPRETER_H
 #define PROJEKAT2526_INTERPRETER_H
-
+#include <iostream>
+#include "tokenizer/Tokenizer.h"
+#include "parser/Parser.h"
+#include "error/Error.h"
 
 class Interpreter {
+private:
+    Tokenizer &tokenizer;
+    Parser &parser;
+    std::string prompt;
+
+    static bool is_blank(const std::string &string);
+public:
+    Interpreter(Tokenizer &tokenizer, Parser &parser, std::string prompt = "$");
+    void set_prompt(const std::string &new_prompt);
+
+    void run();
+
+    static void print_error(const Error &error);
 };
 
 
-#endif //SORTING_INTERPRETER_H
+#endif //PROJEKAT2526_INTERPRETER_H
