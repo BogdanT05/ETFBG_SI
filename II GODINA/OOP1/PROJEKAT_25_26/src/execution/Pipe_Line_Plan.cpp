0dd6f2006f098410727fc3b1d15acba3f2c38f01
Bogdan <bogdantodorovic005@gmail.com>
2026-01-29 23:09:42 +0100

    Implement Pipe Line Plan

--- II GODINA/OOP1/PROJEKAT_25_26/src/execution/Pipe_Line_Plan.cpp
+++ II GODINA/OOP1/PROJEKAT_25_26/src/execution/Pipe_Line_Plan.cpp
@@ -1,1 +1,36 @@
 #include "Pipe_Line_Plan.h"
+
+#include "Pipe_Buffer.h"
+#include "Pipe_Input_Stream.h"
+#include "Pipe_Output_Stream.h"
+
+Pipe_Line_Plan::Pipe_Line_Plan(std::vector<std::unique_ptr<Command>> commands) : commands(std::move(commands)){}
+
+void Pipe_Line_Plan::execute() {
+    if (commands.size() == 1) {
+        commands[0]->execute();
+        return;
+    }
+
+    std::vector<std::unique_ptr<Pipe_Buffer>> buffers;
+    buffers.reserve(commands.size()-1);
+
+    for (std::size_t i = 0; i < commands.size()-1; i++) {
+        buffers.push_back(std::make_unique<Pipe_Buffer>());
+    }
+
+    for (std::size_t i = 0; i < commands.size(); i++) {
+        if (i > 0) {
+            auto *in = new Pipe_Input_Stream(buffers[i-1].get());
+            commands[i]->set_input(in);
+        }
+
+        if (i < commands.size()-1) {
+            auto *out = new Pipe_Output_Stream(buffers[i].get());
+            commands[i]->set_output(out);
+        }
+    }
+
+    for (const auto & command : commands)
+        command->execute();
+}
--- II GODINA/OOP1/PROJEKAT_25_26/src/execution/Pipe_Line_Plan.h
+++ II GODINA/OOP1/PROJEKAT_25_26/src/execution/Pipe_Line_Plan.h
@@ -1,8 +1,18 @@
 #ifndef PROJEKAT2526_PIPE_LINE_PLAN_H
 #define PROJEKAT2526_PIPE_LINE_PLAN_H
+#include <memory>
+#include <vector>
+
+#include "Command.h"
+#include "Execution_Plan.h"
 
 
-class Pipe_Line_Plan {
+class Pipe_Line_Plan : public Execution_Plan{
+private:
+    std::vector<std::unique_ptr<Command>> commands;
+public:
+    explicit Pipe_Line_Plan(std::vector<std::unique_ptr<Command>> commands);
+    void execute() override;
 };
 
 
