203e35c46b3bba0f1d313041b8231020ec1bc01e
Bogdan <bogdantodorovic005@gmail.com>
2026-01-29 19:38:49 +0100

    Implement abstract class Command

--- II GODINA/OOP1/PROJEKAT_25_26/src/commands/Command.h
+++ II GODINA/OOP1/PROJEKAT_25_26/src/commands/Command.h
@@ -3,6 +3,23 @@
 
 
 class Command {
+protected:
+    std::string name;
+    std::vector<std::string> arguments;
+    std::vector<std::string> options;
+
+    std::istream *is;
+    std::ostream *os;
+public:
+    Command(std::string name, std::vector<std::string> arguments, std::vector<std::string> options, std::istream *is = &std::cin, std::ostream *os = &std::cout):
+        name(std::move(name)), arguments(std::move(arguments)), options(std::move(options)), is(is), os(os)
+    {}
+
+    virtual void set_input(std::istream *is_) = 0;
+    virtual void set_output(std::ostream *os_) = 0;
+
+    virtual void execute() = 0;
+    virtual ~Command() = default;
 };
 
 
