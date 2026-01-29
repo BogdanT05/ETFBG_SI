8f4a142f58dd90e1d5607ba51b0c3c127bbb9d8c
Bogdan <bogdantodorovic005@gmail.com>
2026-01-29 17:19:44 +0100

    Implement Token class

--- II GODINA/OOP1/PROJEKAT_25_26/src/tokenizer/Token.h
+++ II GODINA/OOP1/PROJEKAT_25_26/src/tokenizer/Token.h
@@ -1,8 +1,23 @@
 #ifndef PROJEKAT2526_TOKEN_H
 #define PROJEKAT2526_TOKEN_H
+#include <string>
+
+enum class Token_type {
+    WORD, OPTION, STRING, PIPE, REDIRECT_IN,
+    REDIRECT_OUT, REDIRECT_APPEND
+};
 
 class Token {
+private:
+    Token_type type;
+    std::string value;
+    int position;
+public:
+    Token(const Token_type type, std::string value, const int position): type(type), value(std::move(value)), position(position) {}
 
+    [[nodiscard]] Token_type get_type() const {return type;}
+    [[nodiscard]] std::string get_value() const {return value;}
+    [[nodiscard]] int get_position() const {return position;}
 };
 
 #endif //PROJEKAT2526_TOKEN_H
