293d7b825dbd017e97a2f86994c031ff6a0c4b35
Bogdan <bogdantodorovic005@gmail.com>
2026-01-29 20:04:13 +0100

    Implement derived class Single Command Plan, trivial class that executes one command

--- II GODINA/OOP1/PROJEKAT_25_26/src/execution/Single_Command_Plan.cpp
+++ II GODINA/OOP1/PROJEKAT_25_26/src/execution/Single_Command_Plan.cpp
@@ -1,1 +1,7 @@
 #include "Single_Command_Plan.h"
+
+Single_Command_Plan::Single_Command_Plan(std::unique_ptr<Command> command) : command(std::move(command)) {}
+
+void Single_Command_Plan::execute() {
+    command->execute();
+}
--- II GODINA/OOP1/PROJEKAT_25_26/src/execution/Single_Command_Plan.h
+++ II GODINA/OOP1/PROJEKAT_25_26/src/execution/Single_Command_Plan.h
@@ -1,8 +1,17 @@
 #ifndef PROJEKAT2526_SINGLE_COMMAND_PLAN_H
 #define PROJEKAT2526_SINGLE_COMMAND_PLAN_H
+#include <memory>
+
+#include "Command.h"
+#include "Execution_Plan.h"
 
 
-class Single_Command_Plan {
+class Single_Command_Plan : public Execution_Plan {
+private:
+    std::unique_ptr<Command> command;
+public:
+    explicit Single_Command_Plan(std::unique_ptr<Command> command);
+    void execute() override;
 };
 
 
