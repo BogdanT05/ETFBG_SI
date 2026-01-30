91c7246d98dd83a96df385ec6555f6b0ba1c5edc
Bogdan <bogdantodorovic005@gmail.com>
2026-01-30 04:13:15 +0100

    Implement Batch Plan

--- II GODINA/OOP1/PROJEKAT_25_26/src/execution/Batch_Plan.cpp
+++ II GODINA/OOP1/PROJEKAT_25_26/src/execution/Batch_Plan.cpp
@@ -1,1 +1,34 @@
 #include "Batch_Plan.h"
+
+#include <algorithm>
+
+#include "Error.h"
+#include "Parser.h"
+#include "Tokenizer.h"
+
+
+Batch_Plan::Batch_Plan(Input_Stream *stream ,Tokenizer &tokenizer, Parser &parser) : file(stream), tokenizer(tokenizer), parser(parser){}
+
+void Batch_Plan::execute() {
+    std::string line;
+    while (file->read_line(line)) {
+        if (line.empty() || is_blank(line)) continue;
+
+        try{
+            std::vector<Token> tokens = Tokenizer::tokenize(line);
+            const auto execution_plan = parser.parse(tokens);
+            execution_plan->execute();
+        }
+        catch (const Error &er) {
+            print_error(er);
+        }
+    }
+}
+
+void Batch_Plan::print_error(const Error &error) {
+    error.print();
+}
+
+bool Batch_Plan::is_blank(const std::string &string) {
+    return std::all_of(string.begin(), string.end(), [](const unsigned char c){return std::isspace(c);});
+}
--- II GODINA/OOP1/PROJEKAT_25_26/src/execution/Batch_Plan.h
+++ II GODINA/OOP1/PROJEKAT_25_26/src/execution/Batch_Plan.h
@@ -1,8 +1,22 @@
 #ifndef PROJEKAT2526_BATCH_PLAN_H
 #define PROJEKAT2526_BATCH_PLAN_H
+#include "Error.h"
+#include "Execution_Plan.h"
+#include "Input_Stream.h"
+#include "Parser.h"
+#include "Tokenizer.h"
 
 
-class Batch_Plan {
+class Batch_Plan : public Execution_Plan{
+private:
+    Input_Stream *file;
+    Tokenizer &tokenizer;
+    Parser &parser;
+    static bool is_blank(const std::string &string);
+public:
+    explicit Batch_Plan(Input_Stream *stream, Tokenizer &tokenizer, Parser &parser);
+    void execute() override;
+    static void print_error(const Error &error);
 };
 
 
