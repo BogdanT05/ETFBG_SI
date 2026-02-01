11cf6cf8bcf491296f5ed2601fc7f06c2d946218
Bogdan <bogdantodorovic005@gmail.com>
2026-02-01 05:16:46 +0100

    Change interpreter class, add output stream, add execute_line method and add simple destructor, all allowing for easier implementation of batch (plan)(parse)

--- II GODINA/OOP1/PROJEKAT_25_26/src/interpreter/Interpreter.cpp
+++ II GODINA/OOP1/PROJEKAT_25_26/src/interpreter/Interpreter.cpp
@@ -6,34 +6,54 @@
 Interpreter::Interpreter(Tokenizer &tokenizer, Parser &parser, std::string prompt) :
 tokenizer(tokenizer),
 parser(parser),
-prompt(std::move(prompt))
+prompt(std::move(prompt)),
+output(new Console_Output_Stream())
 {}
+
+Interpreter::~Interpreter() {
+    delete output;
+}
 
 void Interpreter::set_prompt(const std::string& new_prompt) {
     prompt = new_prompt;
 }
 
-void Interpreter::run() const {
+void Interpreter::execute_line(const std::string &line) {
+    try {
+        auto tokens = tokenizer.tokenize(line);
+        auto execution_plan = parser.parse(tokens);
+        execution_plan->execute(*this);
+    }
+    catch (const Error &er) {
+        print_error(er);
+    }
+}
+
+void Interpreter::run(){
     while (true) {
         std::string raw_command;
         std::cout << prompt;
-        if (!std::getline(std::cin, raw_command)) break;
 
-        if (raw_command.empty() || is_blank(raw_command)) continue;
+        if (!std::getline(std::cin, raw_command))
+            break;
 
-        try{
-            std::vector<Token> tokens = Tokenizer::tokenize(raw_command);
-            auto execution_plan = parser.parse(tokens);
-            execution_plan->execute();
-        }
-        catch (const Error &er) {
-            print_error(er);
-        }
+        if (raw_command.empty() || is_blank(raw_command))
+            continue;
+
+        execute_line(raw_command);
     }
 }
 
 void Interpreter::print_error(const Error &error) {
     error.print();
+}
+
+Output_Stream * Interpreter::get_output() const {
+    return output;
+}
+
+void Interpreter::set_output(Output_Stream *out) {
+    output = out;
 }
 
 bool Interpreter::is_blank(const std::string &string) {
--- II GODINA/OOP1/PROJEKAT_25_26/src/interpreter/Interpreter.h
+++ II GODINA/OOP1/PROJEKAT_25_26/src/interpreter/Interpreter.h
@@ -1,6 +1,8 @@
 #ifndef PROJEKAT2526_INTERPRETER_H
 #define PROJEKAT2526_INTERPRETER_H
 #include <iostream>
+
+#include "Console_Output_Stream.h"
 #include "tokenizer/Tokenizer.h"
 #include "parser/Parser.h"
 #include "error/Error.h"
@@ -10,15 +12,19 @@ private:
     Tokenizer &tokenizer;
     Parser &parser;
     std::string prompt;
+    Output_Stream *output;
 
     static bool is_blank(const std::string &string);
 public:
     Interpreter(Tokenizer &tokenizer, Parser &parser, std::string prompt = "$");
+    ~Interpreter();
     void set_prompt(const std::string &new_prompt);
-
-    void run() const;
-
+    void execute_line(const std::string &line);
+    void run();
     static void print_error(const Error &error);
+
+    [[nodiscard]] Output_Stream* get_output() const;
+    void set_output(Output_Stream* out);
 };
 
 
