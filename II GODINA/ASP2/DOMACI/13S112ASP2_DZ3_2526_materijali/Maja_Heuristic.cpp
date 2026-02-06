2f7d9e44f5e407ecdd14b3c0e6efc65cbcc78b84
Bogdan <bogdantodorovic005@gmail.com>
2026-02-06 17:00:44 +0100

    Add Interface for Maja Heuristic

--- II GODINA/ASP2/DOMACI/13S112ASP2_DZ3_2526_materijali/Maja_Heuristic.cpp
+++ II GODINA/ASP2/DOMACI/13S112ASP2_DZ3_2526_materijali/Maja_Heuristic.cpp
@@ -1,1 +1,12 @@
 #include "Maja_Heuristic.h"
+
+Maja_Heuristic::Maja_Heuristic(int c) : {}
+
+void Maja_Heuristic::bubble_sort(Image *img, int left, int right, SortingDirection direction) {
+}
+
+int Maja_Heuristic::partition(Image *img, int left, int right, SortingDirection direction) {
+}
+
+void Maja_Heuristic::sort(Image *image, SortingDirection direction) {
+}
--- II GODINA/ASP2/DOMACI/13S112ASP2_DZ3_2526_materijali/Maja_Heuristic.h
+++ II GODINA/ASP2/DOMACI/13S112ASP2_DZ3_2526_materijali/Maja_Heuristic.h
@@ -1,9 +1,18 @@
-#ifndef _MAJA_HEURISTIC_H
-#define _MAJA_HEURISTIC_H
+#ifndef MAJA_HEURISTIC_H
+#define MAJA_HEURISTIC_H
+#include "sorting.h"
 
 
-class Maja_Heuristic {
+class Maja_Heuristic : public SortingAlgorithm{
+private:
+    int c;
+    void bubble_sort(Image *img, int left, int right, SortingDirection direction);
+    int partition(Image *img, int left, int right, SortingDirection direction);
+
+public:
+    explicit Maja_Heuristic(int c);
+    void sort(Image *image, SortingDirection direction) override;
 };
 
 
-#endif //_MAJA_HEURISTIC_H
+#endif //MAJA_HEURISTIC_H
