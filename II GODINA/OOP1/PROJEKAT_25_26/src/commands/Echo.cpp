28d2841dd49ba7c4896dc59864ce3aad014c5bb8
Bogdan <bogdantodorovic005@gmail.com>
2026-03-02 09:11:33 +0100

    Implement echo command

--- II GODINA/OOP1/PROJEKAT_25_26/src/commands/Echo.cpp
+++ II GODINA/OOP1/PROJEKAT_25_26/src/commands/Echo.cpp
@@ -1,1 +1,19 @@
 #include "Echo.h"
+#include "Semantic_Error.h"
+
+void Echo::execute() {
+    std::string result;
+    for (std::size_t i = 0; i < arguments.size(); i++) {
+        if (i != arguments.size()-1)
+            result += arguments[i] + " ";
+        else
+            result += arguments[i];
+    }
+
+    os->write_line(result);
+}
+
+void Echo::validate() {
+    if (!options.empty())
+        throw Semantic_Error("echo does not accept options");
+}
--- II GODINA/OOP1/PROJEKAT_25_26/src/commands/Echo.h
+++ II GODINA/OOP1/PROJEKAT_25_26/src/commands/Echo.h
@@ -7,6 +7,7 @@ class Echo : public Command{
 public:
     using Command::Command;
     void execute() override;
+    void validate() override;
 
     void set_input(Input_Stream *is_) override {is = is_;}
     void set_output(Output_Stream *os_) override {os = os_;}
