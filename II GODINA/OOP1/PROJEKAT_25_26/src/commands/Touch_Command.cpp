1fc5b28ef2a15ec0d114dedc549d05c29181c210
Bogdan <bogdantodorovic005@gmail.com>
2026-03-02 11:49:15 +0100

    Implement touch command

---
+++ II GODINA/OOP1/PROJEKAT_25_26/src/commands/Touch_Command.cpp
@@ -0,0 +1,26 @@
+#include "Touch_Command.h"
+#include "Execution_Error.h"
+#include "Semantic_Error.h"
+#include <fstream>
+
+#include "IOError.h"
+
+void Touch::execute(Interpreter &interpreter) {
+    std::ifstream file(arguments[0]);
+    if (file.good())
+        throw Execution_Error("File already exists'" + arguments[0] + "'");
+
+    std::ofstream create(arguments[0]);
+    if (!create)
+        throw IOError("Cannot create file '" + arguments[0] + "'");
+
+    create.close();
+}
+
+void Touch::validate() {
+    if (!options.empty())
+        throw Semantic_Error("touch does not accept options");
+
+    if (arguments.size() != 1)
+        throw Semantic_Error("touch requires exactly one argument");
+}
