8a7fe3381efb23d5de63952d4c2214549ebead2a
Bogdan <bogdantodorovic005@gmail.com>
2026-03-02 06:56:13 +0100

    Implement Syntax_Error

--- II GODINA/OOP1/PROJEKAT_25_26/src/error/Syntax_Error.cpp
+++ II GODINA/OOP1/PROJEKAT_25_26/src/error/Syntax_Error.cpp
@@ -1,1 +1,15 @@
 #include "Syntax_Error.h"
+
+Syntax_Error::Syntax_Error(int position_, int length_) : position_(position_), length_(length_){}
+
+int Syntax_Error::position() const {
+    return position_;
+}
+
+int Syntax_Error::length() const {
+    return length_;
+}
+
+std::string Syntax_Error::message() const {
+    return "Syntax Error";
+}
--- II GODINA/OOP1/PROJEKAT_25_26/src/error/Syntax_Error.h
+++ II GODINA/OOP1/PROJEKAT_25_26/src/error/Syntax_Error.h
@@ -1,8 +1,15 @@
 #ifndef PROJEKAT2526_SYNTAX_ERROR_H
 #define PROJEKAT2526_SYNTAX_ERROR_H
+#include "Error.h"
 
-
-class Syntax_Error {
+class Syntax_Error : public Error{
+    int position_;
+    int length_;
+public:
+    explicit Syntax_Error(int position_, int length_=1);
+    [[nodiscard]]std::string message() const override;
+    [[nodiscard]]int position() const override;
+    [[nodiscard]]int length() const override;
 };
 
 
