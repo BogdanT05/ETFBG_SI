8afd2106ee3da84327d0b0d02264bc97fddbd140
Bogdan <bogdantodorovic005@gmail.com>
2026-03-02 06:28:10 +0100

    Implement Lexical_Error

--- II GODINA/OOP1/PROJEKAT_25_26/src/error/Lexical_Error.cpp
+++ II GODINA/OOP1/PROJEKAT_25_26/src/error/Lexical_Error.cpp
@@ -1,1 +1,20 @@
 #include "Lexical_Error.h"
+#include <utility>
+
+Lexical_Error::Lexical_Error(int position, int length, std::string message) :
+    position_(position),
+    length_(length),
+    message_(std::move(message))
+{}
+
+std::string Lexical_Error::message() const {
+    return message_;
+}
+
+int Lexical_Error::position() const {
+    return position_;
+}
+
+int Lexical_Error::length() const {
+    return length_;
+}
--- II GODINA/OOP1/PROJEKAT_25_26/src/error/Lexical_Error.h
+++ II GODINA/OOP1/PROJEKAT_25_26/src/error/Lexical_Error.h
@@ -1,8 +1,20 @@
 #ifndef PROJEKAT2526_LEXICAL_ERROR_H
 #define PROJEKAT2526_LEXICAL_ERROR_H
+#include "Error.h"
 
 
-class Lexical_Error {
+class Lexical_Error : public Error{
+private:
+    int position_;
+    int length_;
+    std::string message_;
+public:
+
+    Lexical_Error(int position, int length, std::string message);
+    [[nodiscard]] std::string message() const override;
+    [[nodiscard]] int position() const override;
+    [[nodiscard]] int length() const override;
+
 };
 
 
