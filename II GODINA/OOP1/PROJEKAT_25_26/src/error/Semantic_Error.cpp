2f25f71352bcd16a4c8f3a524ea70a0ecd060130
Bogdan <bogdantodorovic005@gmail.com>
2026-03-02 06:28:28 +0100

    Implement Semantic_Error

--- II GODINA/OOP1/PROJEKAT_25_26/src/error/Semantic_Error.cpp
+++ II GODINA/OOP1/PROJEKAT_25_26/src/error/Semantic_Error.cpp
@@ -1,1 +1,9 @@
 #include "Semantic_Error.h"
+
+#include <utility>
+
+Semantic_Error::Semantic_Error(std::string message_) : message_(std::move(message_)){}
+
+std::string Semantic_Error::message() const {
+    return message_;
+}
--- II GODINA/OOP1/PROJEKAT_25_26/src/error/Semantic_Error.h
+++ II GODINA/OOP1/PROJEKAT_25_26/src/error/Semantic_Error.h
@@ -1,8 +1,14 @@
 #ifndef PROJEKAT2526_SEMATIC_ERROR_H
 #define PROJEKAT2526_SEMATIC_ERROR_H
+#include "Error.h"
 
 
-class Semantic_Error {
+class Semantic_Error : public Error{
+private:
+    std::string message_;
+public:
+    explicit Semantic_Error(std::string message_);
+    [[nodiscard]] std::string message() const override;
 };
 
 
