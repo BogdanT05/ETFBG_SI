1366b4b993a09de65fe097028b91a0493506580e
Bogdan <bogdantodorovic005@gmail.com>
2026-03-02 06:44:54 +0100

    Implement Execution_Error

--- II GODINA/OOP1/PROJEKAT_25_26/src/error/Execution_Error.cpp
+++ II GODINA/OOP1/PROJEKAT_25_26/src/error/Execution_Error.cpp
@@ -1,1 +1,7 @@
 #include "Execution_Error.h"
+
+Execution_Error::Execution_Error(std::string &message_) : message_(std::move(message_)){}
+
+std::string Execution_Error::message() const {
+    return message_;
+}
--- II GODINA/OOP1/PROJEKAT_25_26/src/error/Execution_Error.h
+++ II GODINA/OOP1/PROJEKAT_25_26/src/error/Execution_Error.h
@@ -1,8 +1,14 @@
 #ifndef PROJEKAT2526_EXECUTION_ERROR_H
 #define PROJEKAT2526_EXECUTION_ERROR_H
+#include "Error.h"
 
 
-class Execution_Error {
+class Execution_Error : public Error{
+private:
+    std::string message_;
+public:
+    explicit Execution_Error(std::string &message_);
+    [[nodiscard]]std::string message() const override;
 };
 
 
