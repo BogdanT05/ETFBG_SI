59e59b21709981fe3427affd288084dfdea7941f
Bogdan <bogdantodorovic005@gmail.com>
2026-03-02 17:48:13 +0100

    Implement main program

--- II GODINA/OOP1/PROJEKAT_25_26/src/main.cpp
+++ II GODINA/OOP1/PROJEKAT_25_26/src/main.cpp
@@ -1,5 +1,12 @@
 #include "CLI.h"
 
 int main() {
+    Tokenizer tokenizer;
+    Parser parser;
+
+    Interpreter interpreter(tokenizer, parser, "$ ");
+
+    interpreter.run();
+
     return 0;
 }
