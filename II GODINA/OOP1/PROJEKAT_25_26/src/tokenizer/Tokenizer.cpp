eb09bc7e99d5a9671402eb84365af1401c6946f0
Bogdan <bogdantodorovic005@gmail.com>
2026-03-03 16:33:04 +0100

    refactor tokenizer: decompose tokenize() in smaller functions

--- II GODINA/OOP1/PROJEKAT_25_26/src/tokenizer/Tokenizer.cpp
+++ II GODINA/OOP1/PROJEKAT_25_26/src/tokenizer/Tokenizer.cpp
@@ -1,101 +1,104 @@
 #include "Tokenizer.h"
 
+#include <utility>
+#include "Lexical_Error.h"
 std::vector<Token> Tokenizer::tokenize(const std::string &s) {
     std::vector<Token> tokens;
     int i = 0;
     while (i < s.length()){
         char current_char = s[i];
-        if (std::isspace(static_cast<unsigned char>(current_char))) {
+        if (std::isspace(static_cast<unsigned char>(current_char)))
             i++;
-            continue;
-        }
-
-        if (current_char == '"') {
-            int starting_positon = i;
-            std::string word;
-            i++;
-
-            while (i < s.length() && s[i] != '"')
-                word += s[i++];
-
-            if (i == s.length()) throw Lexical_Error(starting_positon, i-starting_positon,
-                "Unterminated string literal");
-            Token token(Token_type::STRING, word, starting_positon);
-            tokens.push_back(token);
-            i++;
-            continue;
-        }
-
-        if (current_char == '|') {
-            Token token(Token_type::PIPE, "|", i);
-            tokens.push_back(token);
-            i++;
-            continue;
-        }
-
-        if (current_char == '>' && i+1 < s.length() && s[i+1] == '>') {
-            Token token(Token_type::REDIRECT_APPEND, ">>", i);
-            tokens.push_back(token);
-            i+=2;
-            continue;
-        }
-
-        if (current_char == '>') {
-            Token token(Token_type::REDIRECT_OUT, ">", i);
-            tokens.push_back(token);
-            i++;
-            continue;
-        }
-
-
-        if (current_char == '<') {
-            Token token(Token_type::REDIRECT_IN, "<", i);
-            tokens.push_back(token);
-            i++;
-            continue;
-        }
-
-        if (current_char == '-') {
-            int starting_positon = i;
-            std::string word;
-            Token_type option = Token_type::OPTION;
-            bool special_arg = false;
-            i++;
-            if (i < s.length() && s[i] == '"') {
-                word += '"';
-                i++;
-                while (i < s.length() && s[i] != '"')
-                    word += s[i++];
-
-                if (i == s.length())
-                    throw Lexical_Error(starting_positon, i-starting_positon, "Unterminated string in option");
-                word += '"';
-                i++;
-                special_arg = true;
-            }
-            else {
-                while (i < s.length() && !std::isspace(static_cast<unsigned char>(s[i])) && s[i] != '|' && s[i] != '"' && s[i] != '<' && s[i] != '>')
-                    word += s[i++];
-            }
-
-            if (special_arg)
-                option = Token_type::STRING;
-
-            Token token(option, "-"+word, starting_positon);
-            tokens.push_back(token);
-
-            continue;
-        }
-
-        int starting_positon = i;
-        std::string word;
-
-        while (i < s.length() && !std::isspace(static_cast<unsigned char>(s[i])) && s[i] != '|' && s[i] != '"' && s[i] != '<' && s[i] != '>')
-            word += s[i++];
-
-        Token token(Token_type::WORD, word, starting_positon);
-        tokens.push_back(token);
+        else if (current_char == '"')
+            string_literal(tokens, s, i);
+        else if (current_char == '|')
+            pipe(tokens, i);
+        else if (current_char == '>' && i+1 < s.length() && s[i+1] == '>')
+            redirect_append(tokens, i);
+        else if (current_char == '>')
+            redirect(tokens, Token_type::REDIRECT_OUT, ">", i);
+        else if (current_char == '<')
+            redirect(tokens, Token_type::REDIRECT_IN, "<", i);
+        else if (current_char == '-')
+            option(tokens, s, i);
+        else
+            word(tokens, s, i);
     }
 
     return tokens;
+}
+
+void Tokenizer::string_literal(std::vector<Token> &tokens, const std::string &s, int &i) {
+    int starting_positon = i;
+    std::string word;
+    i++;
+
+    while (i < s.length() && s[i] != '"')
+        word += s[i++];
+
+    if (i == s.length()) throw Lexical_Error(starting_positon, i-starting_positon,
+        "Unterminated string literal");
+    Token token(Token_type::STRING, word, starting_positon);
+    tokens.push_back(token);
+    i++;
+}
+
+void Tokenizer::pipe(std::vector<Token> &tokens, int &i) {
+    Token token(Token_type::PIPE, "|", i);
+    tokens.push_back(token);
+    i++;
+}
+
+void Tokenizer::redirect_append(std::vector<Token> &tokens, int &i) {
+    Token token(Token_type::REDIRECT_APPEND, ">>", i);
+    tokens.push_back(token);
+    i+=2;
+}
+
+void Tokenizer::redirect(std::vector<Token> &tokens, Token_type type,std::string value, int &i) {
+    Token token(type, std::move(value), i);
+    tokens.push_back(token);
+    i++;
+}
+
+void Tokenizer::option(std::vector<Token> &tokens, const std::string &s, int &i) {
+    int starting_positon = i;
+    std::string word;
+    Token_type option = Token_type::OPTION;
+
+    bool special_arg = false;
+    i++;
+    if (i < s.length() && s[i] == '"') {
+        word += '"';
+        i++;
+        while (i < s.length() && s[i] != '"')
+            word += s[i++];
+
+        if (i == s.length())
+            throw Lexical_Error(starting_positon, i-starting_positon, "Unterminated string in option");
+        word += '"';
+        i++;
+        special_arg = true;
+    }
+    else {
+        while (i < s.length() && !std::isspace(static_cast<unsigned char>(s[i])) && s[i] != '|' && s[i] != '"' && s[i] != '<' && s[i] != '>')
+            word += s[i++];
+    }
+
+    if (special_arg)
+        option = Token_type::STRING;
+
+    Token token(option, "-"+word, starting_positon);
+    tokens.push_back(token);
+}
+
+void Tokenizer::word(std::vector<Token> &tokens, const std::string &s, int &i) {
+    int starting_positon = i;
+    std::string word;
+
+    while (i < s.length() && !std::isspace(static_cast<unsigned char>(s[i])) && s[i] != '|' && s[i] != '"' && s[i] != '<' && s[i] != '>')
+        word += s[i++];
+
+    Token token(Token_type::WORD, word, starting_positon);
+    tokens.push_back(token);
 }
--- II GODINA/OOP1/PROJEKAT_25_26/src/tokenizer/Tokenizer.h
+++ II GODINA/OOP1/PROJEKAT_25_26/src/tokenizer/Tokenizer.h
@@ -1,12 +1,17 @@
 #ifndef PROJEKAT2526_TOKENIZER_H
 #define PROJEKAT2526_TOKENIZER_H
 #include <vector>
-#include "Lexical_Error.h"
 #include "Token.h"
 
 class Tokenizer {
 public:
     static std::vector<Token> tokenize(const std::string &s);
+    static void string_literal(std::vector<Token> &tokens, const std::string &s, int &i);
+    static void pipe(std::vector<Token> &tokens, int &i);
+    static void redirect_append(std::vector<Token> &tokens, int &i);
+    static void redirect(std::vector<Token> &tokens, Token_type type, std::string value, int &i);
+    static void option(std::vector<Token> &tokens, const std::string &s, int &i);
+    static void word(std::vector<Token> &tokens, const std::string &s, int &i);
 };
 
 
