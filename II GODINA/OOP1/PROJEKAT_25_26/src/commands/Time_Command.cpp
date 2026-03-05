554bc55db65cf02822beae05209871a94094ab7e
Bogdan <bogdantodorovic005@gmail.com>
2026-03-05 11:06:17 +0100

    Add options for displaying only hours, minutes or seconds

--- II GODINA/OOP1/PROJEKAT_25_26/src/commands/Time_Command.cpp
+++ II GODINA/OOP1/PROJEKAT_25_26/src/commands/Time_Command.cpp
@@ -3,18 +3,25 @@
 #include <iomanip>
 #include <ctime>
 #include <sstream>
-
 #include "Console_Input_Stream.h"
 #include "File_Input_Stream.h"
-#include "Interpreter.h"
 
 void Time::execute(Interpreter &interpreter) {
     std::time_t now = std::time(nullptr);
     std::tm* local = std::localtime(&now);
-
     std::ostringstream oss;
-    oss << local->tm_hour << ":" << std::setw(2) << std::setfill('0')
-        << local->tm_min << ":" << std::setw(2) << std::setfill('0') << local->tm_sec;
+
+    if (options.empty())
+        oss << local->tm_hour << ":" << std::setw(2) << std::setfill('0')
+            << local->tm_min << ":" << std::setw(2) << std::setfill('0') << local->tm_sec;
+    else {
+        if (options[0][1] == 'h')
+            oss << local->tm_hour << std::setw(2) << std::setfill('0');
+        else if (options[0][1] == 'm')
+            oss << local->tm_min << std::setw(2) << std::setfill('0');
+        else
+            oss << local->tm_sec << std::setw(2) << std::setfill('0');
+    }
 
     os->write_line(oss.str());
 }
@@ -26,7 +33,10 @@ void Time::validate() {
     if (!arguments.empty())
         throw Semantic_Error("time does not accept arguments");
 
-    if (!options.empty()) {
-        throw Semantic_Error("time does not accept options");
+    if (options.size() > 1) {
+        throw Semantic_Error("time accepts at most 1 option");
     }
+
+    if (!options.empty() && options[0][1] != 'h' && options[0][1] != 'm' && options[0][1] != 's')
+        throw Semantic_Error("Invalid option for time");
 }
