2e4b3bbdb15adf490f8318fb59f896f632d01da4
Bogdan <bogdantodorovic005@gmail.com>
2026-03-08 19:53:08 +0100

    Finalize project

--- II GODINA/OOP2/ZADACI ZA VEZBU/ZADATAK 2/src/engine/Engine.java
+++ II GODINA/OOP2/ZADACI ZA VEZBU/ZADATAK 2/src/engine/Engine.java
@@ -6,19 +6,37 @@ public class Engine {
     static void main() {
         Character c1 = new Character("Aki", 10);
         Character c2 = new Character("Zaki", 10);
+        Character c3 = new Character("Jocke", 10);
 
         c1.setGun(new Rifle());
-        c2.setGun(new Sniper());
+        c2.setGun(new Grenade());
+        c3.setGun(new Pistols());
 
         int round = 1;
-        while(!c1.is_defeated() && !c2.is_defeated()){
+        while(!(c1.is_defeated() && c2.is_defeated()) &&
+              !(c2.is_defeated() && c3.is_defeated()) &&
+              !(c3.is_defeated() && c1.is_defeated())){
+
             System.out.println("Round - " + round);
 
-            c1.shoot(c2);
-            c2.shoot(c1);
+            if(Math.random() < 0.5){
+                c1.shoot(c2);
+                c1.shoot(c3);
+            }
+
+            if(Math.random() < 0.5){
+                c2.shoot(c1);
+                c2.shoot(c3);
+            }
+
+            if(Math.random() < 0.5){
+                c3.shoot(c1);
+                c3.shoot(c2);
+            }
 
             System.out.println(c1);
             System.out.println(c2);
+            System.out.println(c3);
 
             round++;
         }
