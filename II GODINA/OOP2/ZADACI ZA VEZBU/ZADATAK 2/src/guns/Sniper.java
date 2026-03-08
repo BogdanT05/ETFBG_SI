30ca530f466d755525420f76d87fdb91facba8b3
Bogdan <bogdantodorovic005@gmail.com>
2026-03-08 18:43:46 +0100

    Implement derived class Sniper (simple example of inheritance in java)

---
+++ II GODINA/OOP2/ZADACI ZA VEZBU/ZADATAK 2/src/guns/Sniper.java
@@ -0,0 +1,13 @@
+package guns;
+
+public class Sniper extends Gun {
+
+    public Sniper() {
+        super(5, 0.5);
+    }
+
+    public Sniper(int damage, double precision) {
+        super(damage, precision);
+    }
+
+}
