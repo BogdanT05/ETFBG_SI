ec1a83fe204641ee2ded0ee9fa70a975b22d328d
Bogdan <bogdantodorovic005@gmail.com>
2026-03-08 18:51:44 +0100

    Implement simple game loop

---
+++ II GODINA/OOP2/ZADACI ZA VEZBU/ZADATAK 2/src/engine/Engine.java
@@ -0,0 +1,26 @@
+package engine;
+import characters.Character;
+import guns.*;
+
+public class Engine {
+    static void main() {
+        Character c1 = new Character("Aki", 10);
+        Character c2 = new Character("Zaki", 10);
+
+        c1.setGun(new Rifle());
+        c2.setGun(new Sniper());
+
+        int round = 1;
+        while(!c1.is_defeated() && !c2.is_defeated()){
+            System.out.println("Round - " + round);
+
+            c1.shoot(c2);
+            c2.shoot(c1);
+
+            System.out.println(c1);
+            System.out.println(c2);
+
+            round++;
+        }
+    }
+}
