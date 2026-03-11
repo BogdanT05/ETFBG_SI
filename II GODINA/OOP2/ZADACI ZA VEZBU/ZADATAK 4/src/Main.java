48db1d05ff4a5463bf60585a9f1b90c5ec1385e8
Bogdan <bogdantodorovic005@gmail.com>
2026-03-11 01:33:41 +0100

    Implement main program loop

--- II GODINA/OOP2/ZADACI ZA VEZBU/ZADATAK 4/src/Main.java
+++ II GODINA/OOP2/ZADACI ZA VEZBU/ZADATAK 4/src/Main.java
@@ -1,2 +1,34 @@
+import bank.ATM;
+import bank.Account;
+import bank.Salary;
+
+import java.util.Scanner;
+
 public class Main {
+    static void main() {
+        Account account = new Account(0);
+
+        ATM atm = new ATM(account);
+        Salary salary = new Salary(account);
+
+        atm.start();
+        salary.start();
+
+        new Scanner(System.in).nextLine();
+        atm.interrupt();
+        salary.interrupt();
+
+        try{
+            atm.join();
+            salary.join();
+        } catch (InterruptedException e) {}
+
+        int totalDeposit = salary.getBalance();
+        int totalWithdraw = atm.getBalance();
+        int balance = account.getBalance();
+
+        System.out.println("Total Deposit: " + totalDeposit);
+        System.out.println("Total Withdraw: " + totalWithdraw);
+        System.out.println("Balance on account:  " + balance);
+    }
 }
