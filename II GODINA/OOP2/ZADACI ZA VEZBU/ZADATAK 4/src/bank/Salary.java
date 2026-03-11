924b4b04344c0a10469dccfff692ee3c95be3e96
Bogdan <bogdantodorovic005@gmail.com>
2026-03-11 01:29:10 +0100

    Implement class ATM extends Thread

--- II GODINA/OOP2/ZADACI ZA VEZBU/ZADATAK 4/src/bank/Salary.java
+++ II GODINA/OOP2/ZADACI ZA VEZBU/ZADATAK 4/src/bank/Salary.java
@@ -1,4 +1,25 @@
 package bank;
+import java.util.Random;
 
-public class Salary {
+public class Salary extends Thread{
+    private final Account account;
+    private int balance = 0;
+
+    public Salary(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.interrupted()){
+            int amount = 1 + new Random().nextInt(100);
+            account.increase(amount);
+            balance += amount;
+            System.out.println("Salary amount: " + amount);
+        }
+    }
+
+    public int getBalance(){
+        return balance;
+    }
 }
